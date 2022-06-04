df4e1f85b40d4a5aaab90de280ec4a505fb16592
cuongbungmo <106877434+cuongbungmo@users.noreply.github.com>
2022-06-04 22:11:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const CONFIG = {
     introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nhaaa'
     
     btnIntro: 'OK',
-    title: 'Phải chăng Anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng Anh đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Cậu yêu tớ có phải không nào',
+    desc: 'Nếu cậu không trả lời mà thoát ra tức là muốn làm vợ tớ rồi đó nhaaa :v ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Nói lí do cậu thích tớ đi <3',
+    btnReply: 'Gửi cho tớ <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰. Yêu cậu 300.000😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
